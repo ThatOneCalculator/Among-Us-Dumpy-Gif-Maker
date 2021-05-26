1c33ff5a1f1aba9d4c409cda75dd61cf29924da3
ThatOneCalculator <kainoa@t1c.dev>
2021-05-26 13:39:49 -0700

    surse code :trollface:

--- bot.py
+++ bot.py
@@ -78,7 +78,7 @@ class HelpCommand(commands.Cog):
 
 	@commands.command(name="help")
 	async def help_(self, ctx):
-		embed = discord.Embed(title="My commands!", description="Made by ThatOneCalculator and Pixer! [See my source code!](https:/github.com/ThatOneCalculator/Among-Us-Dumpy-Gif-Maker)", color=0xFFE40C)
+		embed = discord.Embed(title="My commands!", description="Made by ThatOneCalculator and Pixer! See my source code: https:/github.com/ThatOneCalculator/Among-Us-Dumpy-Gif-Maker)", color=0xFFE40C)
 		embed.add_field(name="`!!ping`", value="Pings the bot")
 		embed.add_field(name="`!!invite`", value="Invite link for the bot")
 		embed.add_field(name="`!!literallynobot`", value="Directs you to ThatOneCalculator's main bot LiterallyNoBot")
