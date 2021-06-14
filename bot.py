3e5ef3988bfe0d1b3161e6b455efb9003c672ece
ThatOneCalculator <kainoa@t1c.dev>
2021-06-13 23:25:28 -0700

    Fix + in !!tall

--- bot.py
+++ bot.py
@@ -201,7 +201,7 @@ class TheStuff(commands.Cog):
 		if number > 20:
 			return await ctx.send("That's too tall, you sussy baka!")
 		lb = "\n"
-		await ctx.send(f"<:tallamongus_1:853680242124259338>\n{('<:tallamongus_2:853680316110602260>' + lb) * number} + <:tallamongus_3:853680372554268702>")
+		await ctx.send(f"<:tallamongus_1:853680242124259338>\n{('<:tallamongus_2:853680316110602260>' + lb) * number}<:tallamongus_3:853680372554268702>")
 
 	@commands.cooldown(1, 5, commands.BucketType.user)
 	@commands.command(aliases=["twerk", "amogus"])
