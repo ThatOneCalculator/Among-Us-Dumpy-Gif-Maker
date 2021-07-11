14dc6910071d7e2c20dd83244b9838e429c831a7
ThatOneCalculator <kainoa@t1c.dev>
2021-07-11 08:28:45 -0700

    Fix command output

--- src/main/java/dev/t1c/dumpy/sus.java
+++ src/main/java/dev/t1c/dumpy/sus.java
@@ -228,8 +228,7 @@ public class sus {
 
 		// Combines frames into final GIF
 		System.out.println("Converting....");
-		runCmd("convert -delay 1x20 -loop 0 -alpha set -dispose previous " + dotSlash + "F_*" + extraoutput + ".png  "
-				+ output);
+		runCmd("convert -dispose previous -delay 1x20 -loop 0 -alpha set" + dotSlash + "F_*" + extraoutput + ".png " + output);
 		boolean win = isWindows();
 
 		String[] filenames = new String[] { dotSlash + "F_0" + extraoutput + ".png",
